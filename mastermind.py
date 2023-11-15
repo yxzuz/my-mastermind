41901f45c3e59b802e5c8b8e8eea9a278cf686ba
Natcha Ranchan <natcha.ran@ku.th>
2023-11-15 19:04:58 +0700

    class user (setter and getter), user set_up, play

---
+++ mastermind.py
@@ -0,0 +1,119 @@
+import random
+class Board:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class User:
+    def __init__(self, x=4, y=8, allow_dup='no'):
+        self._x = x
+        self._y = y
+        self._allow_dup = allow_dup
+
+    def get_x(self):
+        """"getter x called"""
+        return self._x
+
+    def set_x(self, x):
+        """setter x called"""
+        if isinstance(x, int):
+            self._x = x
+        else:
+            print('Please enter valid value(int)')
+
+    def get_y(self):
+        """"getter y called"""
+        return self._y
+
+    def set_y(self, y):
+        """"setter y called"""
+        if isinstance(y, int):
+            self._y = y
+        else:
+            raise TypeError
+
+    def get_allow_dup(self):
+        """getter allow_dup called"""
+        return self._allow_dup
+
+    def set_allow_dup(self, allow_dup):
+        """"setter allow_dup called"""
+        if isinstance(allow_dup, str) and allow_dup in ['yes','no']:
+            self._allow_dup = allow_dup
+        else:
+            raise TypeError
+    x = property(get_x, set_x)
+    y = property(get_y, set_y)
+    allow_dup = property(get_allow_dup,set_allow_dup)
+
+
+
+    def set_up(self):
+        print(f'Rn your x(colors) is {self.x} and y(positions) is {self.y}.')
+        print(f'Allow duplicate setting is {self.allow_dup}.')
+        print('+++++++++++')
+
+        pick = input('What do you want to change?(x/y/allow_dup): ')
+        if pick not in ['x','y','allow_dup']:
+            while True:
+                print('Please enter valid choice ')
+                pick = input('What do you want to change?(x/y/allow duplicate): ')
+                if pick in ['x','y','allow_dup']:
+                    break
+        elif pick in ['x','y','allow_dup']:
+            if pick == 'x':
+                print('setting x')
+                x = int(input('x = '))
+                self.set_x(x)
+                print('RN x status: ', self.x)
+            elif pick == 'y':
+                print('setting y')
+                y = int(input('y = '))
+                self.set_y(y)
+                print('RN y status: ', self.y)
+            elif pick == 'allow_dup':
+                print('setting allow_dup')
+                allow_dup = input('allow_dup = ')
+                self.set_allow_dup(allow_dup)
+                print('RN allow_dup status: ', self.allow_dup)
+            #might have to fix setter later with value type
+
+    def play(self):
+        print('--Welcome to Play Mastermind--')
+        rules = int(input('DO YOU KNOW HOW TO PLAY?????:0, Yes(1)/No(2) '))
+        if rules == 2:
+            print("""Simple, all you have to do is...
+                """)
+        while True:
+            pick = int(input('Do you want to set up your board?, Yes(1)/No(2) '))
+            if pick == 1:
+                self.set_up()
+                check = int(input('Are you satisfied with your setup?, Yes(1)/No(2) '))
+                while True:
+                    self.set_up()
+                    if check == 2:
+                        break
+            else:
+                break
+        print('ok let\'s start!')
+
+
+
+
+
+
+
+
+
+
+# user
+# x = int(input('Enter color range from 1-8: '))
+# y = int(input('Enter positions range from 1-8: '))
+# game = Board(x,y)
+# game setup
+game = User()
+game.play()
+
+
+# print(f'Playing Mastermind with {game.x} and {game.y} positions')
+# guess = int(input('What is your guess?: '))
