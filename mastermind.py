f38fefd07e19e08f0cf289a25906779683b63265
Natcha Ranchan <natcha.ran@ku.th>
2023-11-19 23:38:46 +0700

    fixed hint and allow dup

--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,9 @@
 import random
 
-class User:
-    def __init__(self, x=6, y=4, allow_dup='no'): #allow dup
+
+
+class Mastermind:
+    def __init__(self, x=6, y=4, allow_dup='yes'): #allow dup
         self.x = x
         self.y = y
         self.allow_dup = allow_dup
@@ -47,44 +49,26 @@ class User:
             raise TypeError
 
 
-
-    # def allow_dup_on(self):
-    #     self.sol()
-    #
-    # def allow_dup_off(self):
-    #     pass
-
     def __check(self,guess):
         my_list = []
         for i in guess:
-            # temp = []
-            # temp.extend([i, int(guess[i])])
             my_list.append(i)
         # print('my_list',my_list)
-
+        print(my_list,self.__my_sol)
         if my_list == self.__my_sol:
             return True
         # print(my_list)
 
-    def __hint(self,guess):
+    def __hint(self, guess):
         # self.check(guess)
         my_list = []
         for i in range(len(guess)):
-            temp = []
-            # temp.extend([i, int(guess[i])])
             my_list.append(str(guess[i]))
-        # print('my_lisy',my_list)
-        # print('my_sol', self.my_sol)
-        # print(self.my_sol[0][1])
-        #y =index 0 x =index1
+
         hint = ''
-        # my_hint = []
+
 
         for i in range(len(self.__my_sol)):
-            # print('i',i)
-            # print(my_list[j],self.my_sol[i])
-            # print(i,my_list[i:len(self.my_sol)],self.my_sol)
-            # , self.my_sol[i:len(self.my_sol)]
             if self.__my_sol[i] == my_list[i]:
                 hint += '*'
             elif my_list[i:len(self.__my_sol)][0] in self.__my_sol:
@@ -111,64 +95,48 @@ class User:
                 # my_list.append(color)
                 # my_list.extend([index,color])
                 self.__my_sol.append(str(color))
-            print(self.__my_sol)
+            # print(self.__my_sol)
         else:
             # print('no dup')
-            temp = []
-            for index in range(self.y):
-                # my_list = []  # temp
+            while True:
+                if len(self.__my_sol) == self.y:
+                    break
                 color = random.randint(1, self.x)
-                # my_list.extend([index,color])
-                while True:
-                    if len(temp) == self.y:
-                        break
-                    if color not in temp:
-                        temp.append(color)
-                    else:
-                        color = random.randint(1, self.x)
-
-
-            print(temp)
-        #     for i in range(len(temp)):
-        #         # if temp[i] in self.my_sol:
-        #         while temp[i] in self.my_sol:
-        #             color = random.randint(1, self.x)
-        #             temp[i] = color
-        #             self.my_sol.append(temp[i])
-        #
-        #             # while z not in self.my_sol:
-        #             #     z = random.randint(1, self.x)
-        #             #     temp[i] = z
-        #                 # if z not in self.my_sol:
-        #                 #     self.my_sol.append(z)
-        #                 #     break
-        #         self.my_sol.append(temp[i])
-        # print(self.my_sol)
-        # print(self.my_sol)
-
-
-            # print(self.my_sol)
+                if color not in self.__my_sol:
+                    self.__my_sol.append(str(color))
 
 
 
-    def set_up(self):
+    def settings(self):
         print(f'Rn your x(colors) is {self.x} and y(positions) is {self.y}.')
         print(f'Allow duplicate setting is {self.allow_dup}.')
         print('+++++++++++')
+
+
+    # def restart(self):
+    #     user_input = int(input('Do you want to play again and restart? Yes(1) / No(2)) '))
+    #
 
 
     def play(self):
         print('--Welcome to Play Mastermind--')
         print(f'Playing Mastermind with {self.x} colors and and {self.y} positions')
         self.__sol()
+        print(self.__my_sol)
+
         count = 0
+        guess = 0
         while True:
             count+=1
             while True:
+
                 guess = input('What is your guess?: ')
+                break
                 if len(guess) == len(self.__my_sol):  # STOP ASKING
                     break
+
             print(f'Your guess is {guess}')
+            self.__check(guess)
             if self.__check(guess): #T LEAVEEEEE
 
                 print('*'*len(guess))
@@ -178,14 +146,26 @@ class User:
             self.__hint(guess)
 
 
-# my_game = Mastermind(6, 4)
-# my_game.play()
+my_game = Mastermind()
+my_game.settings()
+my_game.allow_dup = 'no'
+my_game.play()
 # my_game.summarize()
 
 
 
 
 
+# if __name__ == '__main__':
+#     while True:
+#         my_game = Mastermind()
+#         my_game.play()
+#
+#         user_input = int(input('Do you want to play again and restart? Yes(1) / No(2)) '))
+#         if user_input == 1:
+#             my_game.play()
+#         elif user_input == 2:
+#             break
 
 
 
@@ -195,12 +175,9 @@ class User:
 
 
 
-x = User()
-# x.set_up()
-x.play()
-# user
-# x = int(input('Enter color range from 1-8: '))
-# y = int(input('Enter positions range from 1-8: '))
+
+
+
 # game = Board(x,y)
 # game setup
 # game = User()
